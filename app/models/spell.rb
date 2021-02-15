28b7f07bb7875460d83e2a3ae926e334c4d8c1a7
acsmithweb <aaronsmithweb@gmail.com>
2021-02-15 13:17:20 -0500

    spells controller implemented

--- app/models/spell.rb
+++ app/models/spell.rb
@@ -1,4 +1,14 @@
 class Spell < ApplicationRecord
+  include PgSearch::Model
+
+  pg_search_scope :search,
+    against: [:name, :desc, :higher_level, :range, :components, :classes, :school, :level, :material, :casting_time],
+    using: {
+      tsearch: {
+        prefix: true
+      }
+    }
+
   validates :name, presence: true
   validates :desc, presence: true
   validates :range, presence: true
