38ea9acc96782aa4143cbffeea0f4863a000ad17
Aaron Smith <aaronsmithweb@gmail.com>
2024-09-03 13:55:09 -0400

    Update tavern_emulation_manager.rb

--- app/services/tavern_emulation_manager.rb
+++ app/services/tavern_emulation_manager.rb
@@ -4,7 +4,7 @@ class TavernEmulationManager
   def initiate_chapter(campaign_name, chapter_name)
     chapter_files = Dir.glob("/shared_hdd/My Drive/Dnd Stuff/Campaigns/CampaignNotes/OrlaCampaign/Campaigns/#{campaign_name}/Campaign Chapters/**/*").map{|file| file.gsub("/shared_hdd/My Drive/Dnd Stuff/Campaigns/CampaignNotes/OrlaCampaign/Campaigns/#{campaign_name}/Campaign Chapters/",'') if file.match("#{chapter_name}.md")}.compact
     index_file = chapter_files.select{|file| file if file.match("#{chapter_name}.md")}.first
-    @questline_manager = ChatProfiles::SceneManager.new('AIzaSyDHbWjcsdnhDmr30tKAyrZxDTDv7pRImp8', index_file)
+    @questline_manager = ChatProfiles::SceneManager.new('api_key', index_file)
   end
 
   def questline_manager
